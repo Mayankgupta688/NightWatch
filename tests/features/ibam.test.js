545300553855b427e2da0242c27c4e9b5cf53c86
Mayank Gupta (ARC) <mayank.gupta@iontrading.com>
2019-01-09 13:12:44 +0530

    Extending the Test Cases

--- tests/features/ibam.test.js
+++ tests/features/ibam.test.js
@@ -1,16 +1,23 @@
 module.exports = {
-    'User can Login': function(browser) {
-      browser
-        .url('https://192.168.55.101/scc/login.jsp') 
-        .waitForElementVisible('body', 1000)
-        .verify.title('ION Bank Account Management')
-        .assert.containsText(".loginHeader", 'Login')
-        .setValue("input[name=j_username]", "bluedog.jatinder.virdee@wallstreetsystems.com")
-        .setValue("input[name=j_password]", "123456")
-        .click('input[type=submit]')
-        .waitForElementVisible('#dashboardMainMenu', 1000)
-        .click('#accountsMainMenu')
-        .assert.hidden("[name=_csrf]")
-        .end()
-      }
-  }
+  'User can Login and Access Tab..': function(browser) {
+    browser
+      .url('https://192.168.55.101/scc/login.jsp') 
+      .waitForElementVisible('body', 1000)
+      .verify.title('ION Bank Account Management')
+      .assert.containsText(".loginHeader", 'Login')
+      .setValue("input[name=j_username]", "bluedog.jatinder.virdee@wallstreetsystems.com")
+      .setValue("input[name=j_password]", "123456")
+      .click('input[type=submit]')
+      .waitForElementVisible('#dashboardMainMenu', 1000)
+      .click('#accountsMainMenu')
+      .assert.hidden("[name=_csrf]")
+      .click("#financialInstitutionsMainMenu")
+      .click("#tile-PENDING_REACTIVATE")
+      .click("#internalEntitiesMainMenu")
+      .click("#businessUnitsMainMenu")
+      .click("#authorityGroupsMainMenu")
+      .click("#peopleMainMenu")
+      .click("#bfa")
+      .end()
+    }
+}
